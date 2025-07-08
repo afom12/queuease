2b051813ea3b5243160e936c0af48aa11c3b201b
Afomiya <afomiyaketsella12@gmail.com>
2025-07-08 18:11:34 +0300

    Update main.jsx

--- queuease-frontend/src/main.jsx
+++ queuease-frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import './styles/globals.scss'; // Remove
+import './styles/globals.scss'; //
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
